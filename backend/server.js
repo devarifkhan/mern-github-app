2ddc5795fbb84e5f9588c4f8bf33d258bf77a02d
devarifkhan <arifcse209@gmail.com>
2026-02-23 15:49:15 +0600

    fix: update CORS configuration and modify Vercel rewrites for API routing

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,7 @@ app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: fals
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
-// app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_BASE_URL, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
