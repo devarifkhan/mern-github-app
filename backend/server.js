13709e1359a4ed7d249588b4d4f2aa14057e403b
devarifkhan <arifcse209@gmail.com>
2026-02-23 15:53:44 +0600

    refactor: remove unused path import and static file serving from server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
-import path from "path";
-
 import "./passport/github.auth.js";
 
 import userRoutes from "./routes/user.route.js";
@@ -17,7 +15,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
@@ -30,12 +27,6 @@ app.use(cors({ origin: process.env.CLIENT_BASE_URL, credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 app.listen(PORT, () => {
 	console.log(`Server started on http://localhost:${PORT}`);
